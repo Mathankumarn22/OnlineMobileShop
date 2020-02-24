0d947b7420207b49fa99765f90c0bcf1da3ef725
Admin <Admin@DESKTOP-RLL3JF9>
2020-02-24 11:17:17 +0530

    Add project files.

---
+++ OnlineMobileShop/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using OnlineMobileShop.Entity;
+using OnlineMobileShop.Respository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineMobileShop.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(Account account)
+        {
+            if (ModelState.IsValid ) 
+            {
+                UserRespo.Add(account);
+                return RedirectToAction("LogIn");
+            }
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login([Bind(Include = "MailID, Password")]Account account)
+        {
+            if(UserRespo.Login(account.MailID, account.Password))
+            {
+                return RedirectToAction("DisplayDetails", "Mobile");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }   
+}
